faeb1ea7b8e04e774172ad275b1e854cf03a63c9
Leo Camacho <41972919+theCampel@users.noreply.github.com>
2025-07-05 16:48:10 +0100

    feat: progress baby

--- src/services/DatabaseService.ts
+++ src/services/DatabaseService.ts
@@ -20,10 +20,7 @@ export class DatabaseService {
   }
 
   public async query(text: string, params?: any[]) {
-    const start = Date.now();
     const res = await this.pool.query(text, params);
-    const duration = Date.now() - start;
-    console.log('executed query', { text, duration, rows: res.rowCount });
     return res;
   }
 
